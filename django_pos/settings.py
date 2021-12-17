e1f95691c37c1f83701c4bd94c7cd5af5fa667d6
zahur <zahurmeerun@hotmail.com>
2021-12-17 13:54:15 +0400

    fix: remove secret ket from repo and set up environment variables

--- django_pos/settings.py
+++ django_pos/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+if os.path.exists("env.py"):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sz4fz)f2@m9v&h=be+0f+iwqtf)4zkp5pp$8ohx802ueo-h+xh'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
