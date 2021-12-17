504a8785cddf23032fd5ad989a5b21c2eee715e1
zahur <zahurmeerun@hotmail.com>
2021-12-17 15:49:22 +0400

    feat: add home page with generic template

--- django_pos/settings.py
+++ django_pos/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Required when using MEDIA_URL in template
+                "django.template.context_processors.media",
             ],
         },
     },
@@ -124,7 +127,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+# For main static file not tied up to app and in base_dir/Required for base.css so django will look here too
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+# This will add the file to the media folder and not to another url
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
