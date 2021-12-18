660cd8328413484096606bf69def8c152eb52672
zahur <zahurmeerun@hotmail.com>
2021-12-18 17:35:19 +0400

    add model for products with category and subcategory

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Product, Category, subCategory
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    # what to be displayed in admin
+    list_display = ('id', 'name', 'category', 'subcategory', 'sku', )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+
+class subCategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', )
+
+# The model followed by class name (model, class name)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(subCategory, subCategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    name = models.CharField(max_length=254)
+
+    # Will return the actual name in admin fields
+    def __str__(self):
+        return self.name
+
+class subCategory(models.Model):
+
+    class Meta:
+        verbose_name_plural = "subCategory"
+
+    name = models.CharField(max_length=254)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL, related_name='cat')
+    # category.cat.all: will return all subCategory for that a specific category
+
+    # Will return the actual name in admin fields
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    subcategory = models.ForeignKey('subCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=False, blank=False)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    has_colour = models.BooleanField(default=False, null=True, blank=True)
+    stock_available = models.IntegerField()
+    has_vat = models.BooleanField(default=False, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        """
+        Create SKU on save automatically
+        """
+
+        self.sku = f'{self.category}/{self.subcategory}-{self.id}-'+uuid.uuid4().hex.upper()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.order_number
