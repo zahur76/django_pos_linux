e73f039c87a15646c2223be47300e0d3777ba70d
zahur <zahurmeerun@hotmail.com>
2021-12-18 16:03:24 +0400

    add styling to login modal

--- static/js/base.js
+++ static/js/base.js
@@ -14,5 +14,15 @@ $(document).ready(function(){
         $(".flash-message").hide("slow");
     }, 3000 ); // 5 secs
 
+    /* Allow password to become visible*/ 
+    $("body").on('click', '.eye-icon', function() {
+        $(this).toggleClass("fa-eye fa-eye-slash");
+        input = $("#password");
+        if (input.attr("type") === "password") {
+          input.attr("type", "text");
+        } else {
+          input.attr("type", "password");
+        }
+    });
           
 });
