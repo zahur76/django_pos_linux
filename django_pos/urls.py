4fa127781f5e66605cf50232cb64336f4dfc875e
zahur <zahurmeerun@hotmail.com>
2021-12-19 09:41:08 +0400

    add category adminpage

--- django_pos/urls.py
+++ django_pos/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
     path("login/", include("login.urls")),
+    path("products/", include("products.urls")),
     path("admin_center/", include("admin_center.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Category
 
-# Create your views here.
+
+# Create your views here
+def category(request):
+    """A view to return the index page"""
+
+    categories = Category.objects.all()
+
+    context = {
+        'categories': categories,
+    }
+
+    return render(request, "products/category.html", context)
