b9bbb9170bed29d15734aa076a5a47effb46c525
zahur <zahurmeerun@hotmail.com>
2022-01-13 07:42:34 +0400

    fix: prevent activation of confirm button with empyty basket

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
+from .models import Order, OrderLineItem
+
 # Register your models here.
+
+
+class OrderAdmin(admin.ModelAdmin):
+    # what to be displayed in admin
+    list_display = (
+        "id",
+        "order_number",
+        "created_at",
+    )
+
+
+class OrderLineItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "order",
+    )
+
+
+# The model followed by class name (model, class name)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderLineItem, OrderLineItemAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,66 @@
+import uuid
 from django.db import models
 
+
 # Create your models here.
+
+class Order(models.Model):
+    class Meta:
+        verbose_name_plural = "Orders"
+
+    order_number = models.CharField(max_length=254)
+    created_at = models.DateTimeField(auto_now_add=True)
+    net_total = models.IntegerField()
+    vat = models.IntegerField()
+    gross_total = models.IntegerField()
+
+    # Will return the actual name in admin fields
+    def __str__(self):
+        return self.order_number
+    
+    def save(self, *args, **kwargs):
+        """
+        Create Order Number on save
+        """
+        self.sku = uuid.uuid4().hex.upper()
+        super().save(*args, **kwargs)
+
+
+class Order(models.Model):
+    class Meta:
+        verbose_name_plural = "Orders"
+
+    order_number = models.CharField(max_length=254)
+    created_at = models.DateTimeField(auto_now_add=True)
+    net_total = models.IntegerField()
+    vat = models.IntegerField()
+    gross_total = models.IntegerField()
+
+    # Will return the actual name in admin fields
+    def __str__(self):
+        return self.order_number
+    
+    def save(self, *args, **kwargs):
+        """
+        Create Order Number on save
+        """
+        self.order_number = uuid.uuid4().hex.upper()
+        super().save(*args, **kwargs)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(
+        Order, null=False, blank=False,
+        on_delete=models.CASCADE, related_name='lineitems')
+    product = product = models.ForeignKey(
+        'products.Product', null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(
+        null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(
+        max_digits=6, decimal_places=2)
+    lineItem_vat = models.DecimalField(
+        max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.order.order_number}'
+    
