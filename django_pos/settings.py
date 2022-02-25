ad7aa1144561430089ce1b8be1b57130274bab58
zahur <zahurmeerun@hotmail.com>
2022-02-25 12:58:03 +0400

    add: settings for aws elastic beanstalk

--- django_pos/settings.py
+++ django_pos/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "DEVELOPMENT" in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-pos-env.eba-aevscmhw.us-west-2.elasticbeanstalk.com']
 
