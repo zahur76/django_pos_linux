686e86221af3a4921da12c7ff784af2a564ae22b
zahur <zahurmeerun@hotmail.com>
2022-02-25 13:33:18 +0400

    add: settings for aws elastic beanstalk

--- django_pos/settings.py
+++ django_pos/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 if os.path.exists("env.py"):
     import env
 
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
